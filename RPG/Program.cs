922021faf900cc96fe73f664ea1c7e5f3334927a
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2019-11-26 16:32:16 -0800

    added project RPG

---
+++ RPG/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RPG
+{
+    
+    abstract class Somebody
+    {
+        protected int Health;
+        protected int Damage;
+        public void Take_damage(int damage)
+        {
+            Health -= damage;
+        }
+        public abstract void Attack(Somebody somebody);
+    }
+    class Zombie : Somebody
+    {
+        public override void Attack(Somebody somebody)
+        {
+            Console.WriteLine($"Zombie tattacked with {Damage} points");
+            somebody.Take_damage(Damage);
+        }
+    }
+    class Pidr : Somebody
+    {
+        public override void Attack(Somebody somebody)
+        {
+            Console.WriteLine($"Pidr tattacked with {Damage} points");
+            somebody.Take_damage(Damage);
+        }
+    }
+    class Hero : Somebody
+    {
+        public override void Attack(Somebody somebody)
+        {
+            Console.WriteLine($"Hero tattacked with {Damage} points");
+            somebody.Take_damage(Damage);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
